db86427898b2ce07c1c62ef226d18577739457a8
Ranlens Denck <piptr@protonmail.com>
2025-12-25 23:48:39 -0300

    refactor: update includes/snmp_api.php

--- includes/snmp_api.php
+++ includes/snmp_api.php
@@ -2,22 +2,56 @@
 
 /**
  * SNMP Integration for Network Infrastructure
+ * Supports SNMP v1, v2c, and v3
  */
-function snmp_get_data($host, $community, $version = '2c', $timeout = 1000000, $retries = 1) {
-    if (!function_exists('snmp2_get')) {
-        return null;
-    }
 
+/**
+ * Get basic SNMP data from a device
+ */
+function snmp_get_data($host, $community, $version = '2c', $timeout = 1000000, $retries = 1, $v3_auth = []) {
+    $version = (string)$version;
+    
     $data = [
-        'hostname' => @snmp2_get($host, $community, "1.3.6.1.2.1.1.5.0", $timeout, $retries),
-        'description' => @snmp2_get($host, $community, "1.3.6.1.2.1.1.1.0", $timeout, $retries),
-        'uptime' => @snmp2_get($host, $community, "1.3.6.1.2.1.1.3.0", $timeout, $retries),
+        'hostname' => null,
+        'description' => null,
+        'uptime' => null,
         'interfaces' => []
     ];
 
-    // Clean up strings (SNMP returns strings like 'STRING: "Hostname"')
-    foreach ($data as $key => $val) {
-        if (is_string($val)) {
+    $oids = [
+        'hostname' => "1.3.6.1.2.1.1.5.0",
+        'description' => "1.3.6.1.2.1.1.1.0",
+        'uptime' => "1.3.6.1.2.1.1.3.0"
+    ];
+
+    foreach ($oids as $key => $oid) {
+        $val = null;
+        if ($version === '3') {
+            if (function_exists('snmp3_get')) {
+                $val = @snmp3_get(
+                    $host, 
+                    $v3_auth['user'] ?? '', 
+                    $v3_auth['sec_level'] ?? 'noAuthNoPriv',
+                    $v3_auth['auth_protocol'] ?? 'MD5',
+                    $v3_auth['auth_pass'] ?? '',
+                    $v3_auth['priv_protocol'] ?? 'DES',
+                    $v3_auth['priv_pass'] ?? '',
+                    $oid,
+                    $timeout,
+                    $retries
+                );
+            }
+        } elseif ($version === '1') {
+            if (function_exists('snmpget')) {
+                $val = @snmpget($host, $community, $oid, $timeout, $retries);
+            }
+        } else { // Default to v2c
+            if (function_exists('snmp2_get')) {
+                $val = @snmp2_get($host, $community, $oid, $timeout, $retries);
+            }
+        }
+
+        if ($val !== false && $val !== null) {
             $data[$key] = preg_replace('/^.*: "?|"?$/', '', $val);
         }
     }
@@ -25,37 +59,60 @@ function snmp_get_data($host, $community, $version = '2c', $timeout = 1000000, $
     return $data;
 }
 
-function snmp_get_traffic($host, $community, $version = '2c', $timeout = 1000000, $retries = 1) {
-    if (!function_exists('snmp2_real_walk')) {
-        return ['in' => 0, 'out' => 0];
-    }
-
-    $inOctets = @snmp2_real_walk($host, $community, "1.3.6.1.2.1.2.2.1.10", $timeout, $retries);
-    $outOctets = @snmp2_real_walk($host, $community, "1.3.6.1.2.1.2.2.1.16", $timeout, $retries);
-
-    $totalIn = 0;
-    $totalOut = 0;
-
-    if ($inOctets) {
-        foreach ($inOctets as $val) {
-            $totalIn += (float)preg_replace('/^.*: /', '', $val);
-        }
-    }
-
-    if ($outOctets) {
-        foreach ($outOctets as $val) {
-            $totalOut += (float)preg_replace('/^.*: /', '', $val);
-        }
-    }
-
-    // Convert octets to bits and then to a human-readable format or just return the bits/sec if we had delta
-    // For simplicity in this dashboard, we will return a simulated delta or just the raw sum if it's for locality visualization
-    return [
-        'in' => $totalIn,
-        'out' => $totalOut
+/**
+ * Get traffic stats from a device
+ */
+function snmp_get_traffic($host, $community, $version = '2c', $timeout = 1000000, $retries = 1, $v3_auth = []) {
+    $version = (string)$version;
+    $oids = [
+        'in' => "1.3.6.1.2.1.2.2.1.10",
+        'out' => "1.3.6.1.2.1.2.2.1.16"
     ];
+
+    $results = ['in' => 0, 'out' => 0];
+
+    foreach ($oids as $key => $oid) {
+        $walk = null;
+        if ($version === '3') {
+            if (function_exists('snmp3_real_walk')) {
+                $walk = @snmp3_real_walk(
+                    $host, 
+                    $v3_auth['user'] ?? '', 
+                    $v3_auth['sec_level'] ?? 'noAuthNoPriv',
+                    $v3_auth['auth_protocol'] ?? 'MD5',
+                    $v3_auth['auth_pass'] ?? '',
+                    $v3_auth['priv_protocol'] ?? 'DES',
+                    $v3_auth['priv_pass'] ?? '',
+                    $oid,
+                    $timeout,
+                    $retries
+                );
+            }
+        } elseif ($version === '1') {
+            if (function_exists('snmprealwalk')) {
+                $walk = @snmprealwalk($host, $community, $oid, $timeout, $retries);
+            }
+        } else { // Default to v2c
+            if (function_exists('snmp2_real_walk')) {
+                $walk = @snmp2_real_walk($host, $community, $oid, $timeout, $retries);
+            }
+        }
+
+        if ($walk) {
+            $total = 0;
+            foreach ($walk as $val) {
+                $total += (float)preg_replace('/^.*: /', '', $val);
+            }
+            $results[$key] = $total;
+        }
+    }
+
+    return $results;
 }
 
+/**
+ * Factory for SNMP Client
+ */
 function snmp_get_client($config) {
     return new class($config) {
         private $config;
@@ -66,12 +123,24 @@ function snmp_get_client($config) {
             $results = [];
             foreach ($devices as $device) {
                 $host = $device['host'] ?? '';
+                $version = $device['version'] ?? '2c';
                 $community = $device['community'] ?? 'public';
+                
+                $v3_auth = [
+                    'user'          => $device['v3_user'] ?? '',
+                    'sec_level'     => $device['v3_sec_level'] ?? 'noAuthNoPriv',
+                    'auth_protocol' => $device['v3_auth_proto'] ?? 'MD5',
+                    'auth_pass'     => $device['v3_auth_pass'] ?? '',
+                    'priv_protocol' => $device['v3_priv_proto'] ?? 'DES',
+                    'priv_pass'     => $device['v3_priv_pass'] ?? ''
+                ];
+
                 if ($host) {
                     $results[] = [
                         'ip' => $host,
                         'name' => $device['name'] ?? $host,
-                        'data' => snmp_get_data($host, $community)
+                        'version' => $version,
+                        'data' => snmp_get_data($host, $community, $version, 1000000, 1, $v3_auth)
                     ];
                 }
             }
@@ -79,4 +148,3 @@ function snmp_get_client($config) {
         }
     };
 }
-
